a9c96c499baf331358142aa0899aae2554b31e5d
Abhishekkj35 <abhishekkj6810@gmail.com>
2025-11-27 16:38:39 +0530

    changed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                <p className="text-2xl font-bold text-gradient">RESUME-MIND</p>
             </Link>
             <div className="flex gap-4 items-center">
                 <Link to="/upload" className="primary-button w-fit">
